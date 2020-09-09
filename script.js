a9e1ca1799db3936a4b7ca5f374ce932d74d5efe
Kilroy <jordans1@vt.edu>
2020-09-09 19:44:34 -0400

    Added framework html, js, and css

---
+++ script.js
@@ -0,0 +1,4 @@
+var timer = document.querySelector(".time");
+var question = document.querySelector(".question");
+var welcome = document.querySelector(".welcome");
+var answers = document.querySelector(".answers");
