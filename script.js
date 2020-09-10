4be7b9c83fcd652414e6a457f6c53edef9fd3083
Kilroy <jordans1@vt.edu>
2020-09-09 20:16:38 -0400

    fixed layout of base html page and styling with css

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ var timer = document.querySelector(".time");
 var question = document.querySelector(".question");
 var welcome = document.querySelector(".welcome");
 var answers = document.querySelector(".answers");
+
+var seconds = 75;
+
+function timer() {
+    var timeLeft = setInterval(function() {
+      secondsLeft--;
+      timer.textContent = "";
+      timer.textContent = "Time: " + secondsLeft;
+  
+      if(secondsLeft === 0) {
+      }
+  
+    }, 1000);
+  }
