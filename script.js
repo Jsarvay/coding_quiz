9611be04a6887d52a9dbcecffa5e66b80f12e43f
Kilroy <jordans1@vt.edu>
2020-09-12 20:20:00 -0400

    Added the first question so that it appears when the start button is pressed.

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ var timer = document.querySelector(".time");
 var question = document.querySelector(".question");
 var welcome = document.querySelector(".welcome");
 var answers = document.querySelector(".answers");
+var start = document.querySelector("#start");
 
+//Sets a timer to be used once the start button is hit
 var seconds = 75;
 
 function timeClock() {
     var timerInterval = setInterval(function() {
       seconds--;
-      timer.textContent = "";
       timer.textContent = "Time: " + seconds;
   
       if(seconds === 0) {
@@ -18,8 +19,24 @@ function timeClock() {
     }, 1000);
   };
 
+//function that calls and populates the page with the first question and answers
+function questionOne() {
+  start.remove();
+  welcome.textContent = "";
+  question.textContent = "CSS stands for what?";
+  var oneAnswers = ["Created System Style", "Custom Style Setup", "Cascading Style Sheet", "Cool Style Stuff"];
+  for(var i = 0; i < oneAnswers.length; i++){
+    var button = document.createElement("button");
+    button.setAttribute("class", "btn btn-dark");
+    button.textContent = oneAnswers[i];
+    answers.appendChild(button);
+    console.log(oneAnswers[i]);
+  }
+}
+
 document.getElementById("start").addEventListener("click", function(event){
     event.preventDefault();
     console.log("time");
     timeClock();
+    questionOne();
 });
